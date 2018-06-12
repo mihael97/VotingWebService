f421f9266949daa3aa94741c2a3de898815abd0f
mihael97 <mihael.macuka2@gmail.com>
2018-06-12 23:12:54 +0200

    Final commit

--- src/main/java/hr/fer/zemris/java/Inicijalizacija.java
+++ src/main/java/hr/fer/zemris/java/Inicijalizacija.java
@@ -20,6 +20,7 @@ import javax.servlet.annotation.WebListener;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.mchange.v2.c3p0.DataSources;
 
+import hr.fer.zemris.java.dao.DAOException;
 import hr.fer.zemris.java.dao.sql.SQLData;
 
 /**
@@ -55,6 +56,7 @@ public class Inicijalizacija implements ServletContextListener {
 
 		Properties properties = new Properties();
 		String connectionURL = null;
+		PreparedStatement statement = null;
 		try {
 			properties.load(new FileInputStream(
 					Paths.get(sce.getServletContext().getRealPath("/WEB-INF/dbsettings.properties")).toFile()));
@@ -81,7 +83,7 @@ public class Inicijalizacija implements ServletContextListener {
 			connection = cpds.getConnection();
 			createTables(connection);
 
-			PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM Polls");
+			statement = connection.prepareStatement("SELECT COUNT(*) FROM Polls");
 
 			ResultSet set = statement.executeQuery();
 			set.next();
@@ -92,14 +94,15 @@ public class Inicijalizacija implements ServletContextListener {
 
 			created = false;
 
-			statement.close();
+			set.close();
 		} catch (SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
+			throw new DAOException(e);
 		} finally {
 			try {
+				statement.close();
 				connection.close();
 			} catch (SQLException e) {
-				e.printStackTrace();
+				throw new DAOException(e);
 			}
 		}
 	}
@@ -152,24 +155,36 @@ public class Inicijalizacija implements ServletContextListener {
 	 * @param connection
 	 *            - connection
 	 * @return <code>true</code> if table exists,otherwise <code>false</code>
-	 * @throws SQLException
+	 * @throws DAOException
 	 *             - if exception during checking appears
 	 */
-	private boolean checkIfExists(Connection connection) throws SQLException {
-		DatabaseMetaData metaData = connection.getMetaData();
-		ResultSet set = metaData.getTables(null, null, "Polls", null);
+	private boolean checkIfExists(Connection connection) {
+		DatabaseMetaData metaData;
+		ResultSet set = null;
+		try {
+			metaData = connection.getMetaData();
+			set = metaData.getTables(null, null, "Polls", null);
 
-		while (set.next()) { // we are only checking if table 'Polls' exists. We have another table in
-								// memory,but we create both of them in same time so it is small possibility
-								// that
-								// second would'n exists
-			if (set.getString("TABLE_NAME").toUpperCase().equals("POLLS")) {
+			while (set.next()) { // we are only checking if table 'Polls' exists. We have another table in
+									// memory,but we create both of them in same time so it is small possibility
+									// that
+									// second would'n exists
+				if (set.getString("TABLE_NAME").toUpperCase().equals("POLLS")) {
+					set.close();
+					return true;
+				}
+			}
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		} finally {
+			try {
 				set.close();
-				return true;
+			} catch (SQLException e) {
+				throw new DAOException(e);
 			}
 		}
 
-		set.close();
 		return false;
 	}
 
@@ -178,10 +193,8 @@ public class Inicijalizacija implements ServletContextListener {
 	 * 
 	 * @param connection
 	 *            - connection
-	 * @throws SQLException
-	 *             - if exception during insertion appears
 	 */
-	private void fillTables(Connection connection) throws SQLException {
+	private void fillTables(Connection connection) {
 
 		fillTable(connection, SQLData.POLLSOPTIONS_BANDS);
 		fillTable(connection, SQLData.POLLSOPTIONS_CARS);
@@ -194,32 +207,54 @@ public class Inicijalizacija implements ServletContextListener {
 	 *            - connection
 	 * @param options
 	 *            - data we want to insert
-	 * @throws SQLException
-	 *             - if exception during insertion appears
-	 * @throws IllegalArgumentException
-	 *             - if id(poll ID) is lower than 0
+	 * @throws DAOException
+	 *             - if exception during insertion appears or id is lower than 0
 	 */
-	private void fillTable(Connection connection, List<String> options) throws SQLException {
+	private void fillTable(Connection connection, List<String> options) {
 		String polls = "INSERT INTO Polls(title,message) VALUES(";
 		String pollsOptions = "INSERT INTO PollOptions(optionTitle,optionLink,pollID,votesCount) VALUES(";
 
 		for (int i = 0, id = -1; i < options.size(); i++) {
 			if (i == 0) {
-				Statement statement = connection.createStatement();
-				statement.executeUpdate(polls + options.get(i) + ")", Statement.RETURN_GENERATED_KEYS);
-				ResultSet set = statement.getGeneratedKeys();
-				set.next();
-				id = set.getInt(1);
-				statement.close();
-			} else {
-				if (id < 0) {
-					throw new IllegalArgumentException("Id is " + id + " but must be greather or equal to zero!");
+				Statement statement = null;
+				try {
+					statement = connection.createStatement();
+					statement.executeUpdate(polls + options.get(i) + ")", Statement.RETURN_GENERATED_KEYS);
+					ResultSet set = statement.getGeneratedKeys();
+					set.next();
+					id = set.getInt(1);
+				} catch (SQLException e) {
+					throw new DAOException(e);
+				} finally {
+					try {
+						statement.close();
+					} catch (SQLException e) {
+						throw new DAOException(e);
+					}
+
 				}
 
-				PreparedStatement statement = connection.prepareStatement(pollsOptions + options.get(i) + ")");
-				statement.setInt(1, id);
-				statement.execute();
-				statement.close();
+			} else {
+				if (id < 0) {
+					throw new DAOException("Id is " + id + " but must be greather or equal to zero!");
+				}
+
+				PreparedStatement statement = null;
+
+				try {
+					statement = connection.prepareStatement(pollsOptions + options.get(i) + ")");
+					statement.setInt(1, id);
+					statement.execute();
+				} catch (SQLException e) {
+					throw new DAOException(e);
+				} finally {
+					try {
+						statement.close();
+					} catch (SQLException e) {
+						throw new DAOException(e);
+					}
+				}
+
 			}
 		}
 
